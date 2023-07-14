ed6beb96372c623325ff65e3f381a3a845c668df
LukeyWentToLuki <103791978+LukeyWentToLuki@users.noreply.github.com>
2023-07-14 14:16:41 -0400

    Update gamecode.sh

--- gamecode.sh
+++ gamecode.sh
@@ -1,1 +1,87 @@
+#! /bin/bash
+
+
+sleep 2
+
+echo "Loading Game."
+
+
+sleep 3
+
+echo "Game Loaded"
+
+sleep 2
+
+echo "@LukeyWentToLuki Presents"
+
+sleep 3
+
+echo "A Bash Production"
+
+sleep 4
+
+echo "RPG GAME: SIGHT OF THE CURIOUS"
+
+sleep 3
+
+echo "Loading Config. By the way, it is strongly recommended you play RPG GAME 2."
+
+sleep 2
+
+echo "Loaded. Enjoy"
+
+sleep 2
+
+echo "UH WHATS THAT IN THE DISTANCE"
+
+sleep 3
+
+echo "A Un-dead Dinosaur Skeleton Walks to you. Fight him. Pick a number 0 to 9."
+
+read dino
+
+dino=$(( $RANDOM % 10 ))
+
+if [[ $dino == $dino ]]; then
+
+  echo "You defeated him. Nice."
+
+else
+
+    echo "You died, Try again."
+exit 1
+fi
+
+sleep 3
+
+echo "A man in the distance, walks up to you."
+
+sleep 2
+
+echo "AND BOOM, Stabs you with a butterfly knife in the stomach."
+
+#Ouch that hurts.
+
+echo "You wake up."
+
+sleep 2
+
+echo "???: aye look, the stupid guy woke up."
+
+sleep 2
+
+echo "You vanquished my dinosaur."
+
+sleep 3
+
+echo "Now im gonna vanquish your family"
+
+sleep 2
+
+echo "You: Bro What. He tried attacking me"
+
+sleep 3
+
+
+
 
